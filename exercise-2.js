52e0eb47cf1a00c1aebc049cb42774044d29d693
Paranai Promlerd <promlerd.p@gmail.com>
2024-01-11 17:10:25 +0700

    feat(exercise): add completed exercises

--- exercise-2.js
+++ exercise-2.js
@@ -1,1 +1,32 @@
 // Paste the complete MQL here
+/** **Exercise #2**
+
+ให้เขียน Query โดยมีรายละเอียดดังนี้
+
+ */
+
+//หายอดขายทั้งหมดและจำนวนของ Pizza ในแต่ละ Size
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: "$size",
+      total_amount: { $sum: "$total_price" },
+      total_quantity: { $sum: "$quantity" },
+    },
+  },
+]);
+
+//โดยที่ให้เรียงผลลัพธ์ตามยอดขายที่มากที่สุด
+db.pizzaOrders
+  .aggregate([
+    {
+      $group: {
+        _id: "$size",
+        total_amount: { $sum: "$total_price" },
+        total_quantity: { $sum: "$quantity" },
+      },
+    },
+  ])
+  .sort({
+    total_amount: -1,
+  });
--- exercise-5.js
+++ exercise-5.js
@@ -1,1 +1,19 @@
 // Paste the complete MQL here
+/**
+**Exercise #5**
+
+ให้เขียน Query หายอดขายทั้งหมดในแต่ละเดือนและปี
+ - โดยที่เรียงจากปีและเดือนล่าสุดไปเก่าที่สุด
+*/
+db.pizzaOrders
+  .aggregate([
+    {
+      $group: {
+        _id: { month: "$created_at", year: "$created_at" },
+        total_sales: { $sum: "$total_price" },
+      },
+    },
+  ])
+  .sort({
+    _id: -1,
+  });
