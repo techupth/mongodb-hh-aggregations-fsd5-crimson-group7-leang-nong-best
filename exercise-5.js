862b92d79fab6bac114ad19e79d1e984afec5992
devsomsak <devrleang@gmail.com>
2024-01-11 17:49:11 +0700

    feat:create  mongo-query-aggregations is complete

--- exercise-5.js
+++ exercise-5.js
@@ -1,1 +1,28 @@
-// Paste the complete MQL here
+// - ให้เขียน Query หายอดขายทั้งหมดในแต่ละเดือนของปีนั้นๆ
+//    - โดยที่เรียงจากปีและเดือนล่าสุดไปเก่าที่สุด
+
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: {
+        year: { $year: "$created_at" },
+        month: { $month: "$created_at" },
+      },
+      total_sales: { $sum: "$total_price" },
+    },
+  },
+  {
+    $sort: {
+      "_id.year": -1,
+      "_id.month": -1,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      year: "$_id.year",
+      month: "$_id.month",
+      total_sales: 1,
+    },
+  },
+]);
